128d9bedb76014d6398db666f51144e70bec6120
渡邉 浩樹 <hirokiwatanabe622@gmail.com>
2021-02-11 12:05:36 +0900

    初版

---
+++ Log.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LogRecord
+{
+	class Log : IDisposable
+	{
+		/// <summary>
+		/// ファイルに書き込むログの内容
+		/// using System.Collections.Concurrent;
+		/// </summary>
+		private BlockingCollection<string> msg = new BlockingCollection<string>();
+
+		/// <summary>
+		/// ログTask終了命令用
+		/// </summary>
+		private CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+		/// <summary>
+		/// 書込みTaskの実体(Task終了検知)
+		/// </summary>
+		private Task taskWait;
+
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		/// <param name="filename">保存するファイル名</param>
+		internal Log(string filename)
+		{
+			logWrite(AppDomain.CurrentDomain.BaseDirectory + filename);
+			// 例外を出さないようにシフトJISのEncodingを取得
+			Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+		}
+
+		/// <summary>
+		/// デストラクタ
+		/// もしかしてDisposeが実行し忘れてるかもしれないので念のため
+		/// </summary>
+		~Log()
+		{
+			tokenSource.Cancel();
+			taskWait.Wait();
+			tokenSource = null;
+			msg.Dispose();
+			msg = null;
+		}
+
+		/// <summary>
+		/// 終了時の処理
+		/// </summary>
+		public void Dispose()
+		{
+			tokenSource.Cancel();
+			taskWait.Wait();
+			tokenSource = null;
+			msg.Dispose();
+			msg = null;
+			// これでデストラクタは呼ばれなくなるので無駄な終了処理がなくなる
+			GC.SuppressFinalize(this);
+		}
+
+		/// <summary>
+		/// ログの追加
+		/// </summary>
+		/// <param name="message">書き込むメッセージ</param>
+		internal void Message(string message)
+		{
+			_ = msg.TryAdd($"{DateTime.Now:yyyy/M/d HH:mm:ss,}{message}", Timeout.Infinite);
+		}
+
+		/// <summary>
+		///  ログを記録
+		/// </summary>
+		/// <param name="message">エラー内容</param>
+		private void logWrite(string fileName)
+		{
+			// 最初にファイルの有無を確認
+			if (File.Exists(fileName) == false)
+			{
+				// ファイルが存在してなければ作る
+				using var hStream = File.Create(fileName);
+				// 作成時に返される FileStream を利用して閉じる
+				hStream?.Close();
+			}
+
+			taskWait = Task.Run(async () =>
+			{
+				while (true)
+				{
+					try
+					{
+						_ = msg.TryTake(out string log, -1, tokenSource.Token);
+						while (true)
+						{
+							try
+							{
+								// ファイルがロックされている場合例外が発生して以下の処理は行わずリトライとなる
+								using (var stream = new FileStream(fileName, FileMode.Open)) { }
+								// ログ書き込み
+								using var fs = new FileStream(fileName, FileMode.Append, FileAccess.Write, FileShare.ReadWrite);
+								using var sw = new StreamWriter(fs, Encoding.GetEncoding("Shift-JIS"));
+								sw.WriteLine(log);
+								break;
+							}
+							catch
+							{
+								await Task.Delay(1000);
+							}
+						}
+					}
+					catch (OperationCanceledException)
+					{
+						break;
+					}
+				}
+			});
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using LogRecord;
+
+namespace LogTest
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			using Log Test = new(@"\Log.txt");
+			for (int i = 0; i < 1000; i++)
+			{
+				Test.Message(i.ToString());
+			}
+			// 忙しい処理
+			await Task.Delay(12000);
+		}
+	}
+}
